2eadd32f167e1de1a0f242aebfe944a428975c00
GauravChandel <gaurav7217444@gmail.com>
2023-12-20 10:40:57 +0530

    testing file changes

--- cypress/e2e/Testing/testing.cy.js
+++ cypress/e2e/Testing/testing.cy.js
@@ -7,6 +7,10 @@ describe('Learn aboubt read and write',()=>{
         cy.writeFile('test.txt', 'Hello this data is for testing \n')
         cy.writeFile('test.txt', 'Adding a new line', {flag:"a+"})   // if you want to add more details in same file
         cy.writeFile('test.json',{data:'1', data2:'2'})   // if you are creating the file fot json 
-        cy.writeFile('cypress/downloads/test.json',{hi:'hey'})  // if you want to create  the file at particular space
+        cy.writeFile('cypress/downloads/test.json',{hi:'hey'})  // if you want to create  the file at particular space\
+        slkbflkjsdbl
+
+        sdbfjksdbfj
+        sdkfnsdkf
     })
 })
